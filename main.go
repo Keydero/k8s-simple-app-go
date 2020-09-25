74e9c7740f594841825a129916d74d8062d0b1ad
keyproco <keddarts@gmail.com>
2020-09-26 00:30:24 +0300

    first http server with go

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import ( 
+	"fmt"
+	"log"
+	"net/http"
+)
+
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello World")
+}
+
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":80", nil))
+}
+
+
